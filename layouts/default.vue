e0bcf2fd6028793d006beb3bbbbccc473f4f05f0
Pascal Heidmann <pascal@heidmann.io>
2023-04-10 13:48:17 +0200

    No pointer events for current slide overlay

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,3 +1,7 @@
+<script setup>
+import SlideCurrentNo from "@slidev/client/builtin/SlideCurrentNo.vue";
+import SlidesTotal from "@slidev/client/builtin/SlidesTotal.vue";
+</script>
 <template>
   <div>
     <div class="slidev-layout default">
@@ -10,23 +14,3 @@
     </div>
   </div>
 </template>
-<style type="text/less">
-.slidev-current-slide-overlay {
-  display: grid;
-  position: absolute;
-  width: 100%;
-  height: 100%;
-  top: 0;
-  justify-content: center;
-  align-content: end;
-  font-size: .8rem;
-
-  & > span {
-    padding: 1rem;
-  }
-}
-</style>
-<script setup>
-import SlideCurrentNo from "@slidev/client/builtin/SlideCurrentNo.vue";
-import SlidesTotal from "@slidev/client/builtin/SlidesTotal.vue";
-</script>
--- layouts/intro.vue
+++ layouts/intro.vue
@@ -1,3 +1,7 @@
+<script setup>
+import SlideCurrentNo from "@slidev/client/builtin/SlideCurrentNo.vue";
+import SlidesTotal from "@slidev/client/builtin/SlidesTotal.vue";
+</script>
 <template>
   <div>
     <div class="slidev-layout intro">
@@ -13,23 +17,3 @@
     </div>
   </div>
 </template>
-<style type="text/less">
-.slidev-current-slide-overlay {
-  display: grid;
-  position: absolute;
-  width: 100%;
-  height: 100%;
-  top: 0;
-  justify-content: center;
-  align-content: end;
-  font-size: .8rem;
-
-  & > span {
-    padding: 1rem;
-  }
-}
-</style>
-<script setup>
-import SlideCurrentNo from "@slidev/client/builtin/SlideCurrentNo.vue";
-import SlidesTotal from "@slidev/client/builtin/SlidesTotal.vue";
-</script>
